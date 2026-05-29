4fd81855371991366db15ee9eed21ec160e0684d
Abhishek Sinha <sinhaabhishek680@gmail.com>
2026-05-29 16:54:56 -0400

    Add single_square.py: modified rect optimization to always produce a square

--- apps/single_square.py
+++ apps/single_square.py
@@ -7,6 +7,10 @@ import numpy as np
 pydiffvg.set_use_gpu(torch.cuda.is_available())
 
 canvas_width, canvas_height = 256 ,256
+
+# The target square
+# p_min is the top-left corner
+# p_max is the bottom-right corner
 rect = pydiffvg.Rect(p_min = torch.tensor([40.0, 40.0]),
                      p_max = torch.tensor([160.0, 160.0]))
 shapes = [rect]
@@ -25,16 +29,16 @@ img = render(256, # width
              None, # background_image
              *scene_args)
 # The output image is in linear RGB space. Do Gamma correction before saving the image.
-pydiffvg.imwrite(img.cpu(), 'results/single_rect/target.png', gamma=2.2)
+pydiffvg.imwrite(img.cpu(), 'results/single_square/target.png', gamma=2.2)
 target = img.clone()
 
 # Move the rect to produce initial guess
 # normalize p_min & p_max for easier learning rate
 p_min_n = torch.tensor([80.0 / 256.0, 20.0 / 256.0], requires_grad=True)
-p_max_n = torch.tensor([100.0 / 256.0, 60.0 / 256.0], requires_grad=True)
+p_max_n = torch.tensor(100.0 / 256.0, requires_grad=True) # Initial size is 100 px
 color = torch.tensor([0.3, 0.2, 0.5, 1.0], requires_grad=True)
 rect.p_min = p_min_n * 256
-rect.p_max = p_max_n * 256
+rect.p_max = p_max_n * 256 + rect.p_min # Ensures width = height
 rect_group.fill_color = color
 scene_args = pydiffvg.RenderFunction.serialize_scene(\
     canvas_width, canvas_height, shapes, shape_groups)
@@ -45,7 +49,7 @@ img = render(256, # width
              1,   # seed
              None, # background_image
              *scene_args)
-pydiffvg.imwrite(img.cpu(), 'results/single_rect/init.png', gamma=2.2)
+pydiffvg.imwrite(img.cpu(), 'results/single_square/init.png', gamma=2.2)
 
 # Optimize for radius & center
 optimizer = torch.optim.Adam([p_min_n, p_max_n, color], lr=1e-2)
@@ -55,7 +59,7 @@ for t in range(100):
     optimizer.zero_grad()
     # Forward pass: render the image.
     rect.p_min = p_min_n * 256
-    rect.p_max = p_max_n * 256
+    rect.p_max = p_max_n * 256 + rect.p_min
     rect_group.fill_color = color
     scene_args = pydiffvg.RenderFunction.serialize_scene(\
         canvas_width, canvas_height, shapes, shape_groups)
@@ -67,7 +71,7 @@ for t in range(100):
                  None, # background_image
                  *scene_args)
     # Save the intermediate render.
-    pydiffvg.imwrite(img.cpu(), 'results/single_rect/iter_{}.png'.format(t), gamma=2.2)
+    pydiffvg.imwrite(img.cpu(), 'results/single_square/iter_{}.png'.format(t), gamma=2.2)
     # Compute the loss function. Here it is L2.
     loss = (img - target).pow(2).sum()
     print('loss:', loss.item())
@@ -97,10 +101,10 @@ img = render(256,   # width
              None, # background_image
              *scene_args)
 # Save the images and differences.
-pydiffvg.imwrite(img.cpu(), 'results/single_rect/final.png')
+pydiffvg.imwrite(img.cpu(), 'results/single_square/final.png')
 
 # Convert the intermediate renderings to a video.
 from subprocess import call
 call(["ffmpeg", "-framerate", "24", "-i",
-    "results/single_rect/iter_%d.png", "-vb", "20M",
-    "results/single_rect/out.mp4"])
+    "results/single_square/iter_%d.png", "-vb", "20M",
+    "results/single_square/out.mp4"])
