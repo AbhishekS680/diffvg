46631b5d307520ef6191b1b4d94aef418e020dc1
Abhishek Sinha <sinhaabhishek680@gmail.com>
2026-08-07 01:12:16 -0400

    Fix CMake Python detection for pybind11 v2.11.1 (add PYTHON_EXECUTABLE)

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ import platform
 import subprocess
 import importlib
 from sysconfig import get_paths
-
 import importlib
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
@@ -27,7 +26,6 @@ class Build(build_ext):
         except OSError:
             raise RuntimeError("CMake must be installed to build the following extensions: " +
                                ", ".join(e.name for e in self.extensions))
-
         super().run()
 
     def build_extension(self, ext):
@@ -37,8 +35,10 @@ class Build(build_ext):
             include_path = info['include']
             cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
                           '-DPYTHON_LIBRARY=' + get_config_var('LIBDIR'),
-                          '-DPYTHON_INCLUDE_PATH=' + include_path]
-
+                          '-DPYTHON_INCLUDE_PATH=' + include_path,
+                          '-DPYTHON_EXECUTABLE=' + sys.executable,
+                          '-DPython_EXECUTABLE=' + sys.executable,
+                          '-DPython_ROOT_DIR=' + sys.prefix]
             cfg = 'Debug' if self.debug else 'Release'
             build_args = ['--config', cfg]
 
